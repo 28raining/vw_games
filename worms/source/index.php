b0f3ea05fc0281f2c687b908e28d4fef785b70a6
vladimircr <62848823+vladimircr@users.noreply.github.com>
2020-04-05 22:48:12 +0100

    Update index.php
    Added comments to the code. To help better identify the idea behind it. Might have to change if we use realtime database, but this is the most optimum speed to get data from a server and use it in a session which updates the server.

--- worms/source/index.php
+++ worms/source/index.php
@@ -1,6 +1,7 @@
 <?php
+//would contain server call and authentification to the database / if we use.
 include ('_php/??');
-
+//call php function that generates a data array / call from server in this case / from previous work
 $images = ("images");
 ?>
 
@@ -8,7 +9,8 @@ $images = ("images");
 <head>
 
 <script type='text/javascript'>
-//initialise data database
+//initialise data database / json / helps to get the php into javascript array / onload of the page / if we use the google realtime database might be different call /
+//please go to www.cretuvadim.ro -> to see an implementation of how this works and what it does, check the head. You'll see that the server is called only on page load -> i'm quite proud of this implementation :D
 <?php
 $data = json_encode($images);
 echo "var data_js = ". $data . ";\n";
@@ -20,6 +22,7 @@ echo "var data_js = ". $data . ";\n";
 <link href="_css/index.css" rel="stylesheet" type="text/css" />
 <script type='text/javascript' src="_js/XXX.js"></script>
 
+<!-- will be filled properly for good google indexing, still missing the google analytics and the google verificator-->
 <title>Worms</title>
 <meta name="description" content="" />
 <meta name="keywords" content="" />
@@ -27,9 +30,10 @@ echo "var data_js = ". $data . ";\n";
 <meta http-equiv="content-type" content="text/html;charset=UTF-8" />
 
 </head>
-
+<!-- function to get the current width/height of window and generate local variables for scalable calculation -->
 <body onresize="getSize()" onload="getSize()">
 
+<!-- separate into includes, makes it neater to call, the includes are executed on the server-->
 <div id="header">
 	<?php include 'menu.php'; ?>
 </div>
